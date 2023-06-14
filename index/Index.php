1b7f48fe17244f73ed1148b9ed8afaec58123d60
Giovanni Ferreira <9015979@student.zadkine.nl>
2023-06-14 12:34:29 +0200

    2 update

--- index/Index.php
+++ index/Index.php
@@ -1,1 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>FitNess - Home | By Code Info</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- Font Awesome Iocns cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+</head>
+<body>
+<header class="header">
+    <a href="#" class="logo">
+        <i class="fas fa-dumbbell"></i>FitNess
+    </a>
+    <nav class="navbar">
+        <a href="Index.php">Home</a>
+        <a href="services.php">Services</a>
+        <a href="contact.php">Contact</a>
+        <a href="#">|</a>
+        <a href="#">Login</a>
+        <a href="#" class="btn">Sign Up</a>
+    </nav>
+</header>
+
+<section class="home">
+    <div class="max-width">
+        <div class="home-content">
+            <h3>help for ideal <br> body fitness</h3>
+            <p>De beste workouts voor jouw om nog voor de zomer je droomlichaam te krijgen. Ook hebben wij een calorieen calculeter om jouw idaele callorien uittereken</p>
+            <button class="btn">Get started</button>
+        </div>
+    </div>
+    <div class="home-image">
+        <img src="../images/arnold.png" alt="img1"
+    </div>
+</section>
+</body>
+</html>
